0b515d0cb3f35273ef4264ab4631b5d722eeca81
abraham171996 <ibrahimmemmed3131908@gmail.com>
2023-06-19 15:01:26 +0400

    feat:update accordion component

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
 
 const Accordion = ({data}) => {
+    const [curOpen,setCurOpen] = useState(null)
   return (
-    <div className='accordion'>{data.map((el,index)=><AccordionItem
-        title={el.title} text={el.text} num={index} key={Date.now()}
-    />)}</div>
+    <div className='accordion'>{data.map((el,index)=>(<AccordionItem curOpen={curOpen} onOpen = {setCurOpen} title={el.title} num={index} key={el.title}>{el.text}</AccordionItem>))}
+    </div>
   )
 }
 
-function AccordionItem({num,title,text}){
-    const [isOpen,setIsOpen] = useState(false)
+function AccordionItem({num,title,curOpen,onOpen,children}){
+    const isOpen = num === curOpen
     function handleToggle (){
-        setIsOpen(isOpen=>!isOpen)
+        onOpen(isOpen?null:num)
     }
     return(
         <div className={`item ${ isOpen ?"open":""}`} onClick={handleToggle}>
             <p className='number'>{num<9?`0${num+1}`: num+1}</p>
             <p className='text'>{title}</p>
             <p className='icon'>{isOpen ? "-":"+"}</p>
-            {isOpen&&<div className='content-box'>{text}</div>}
+            {isOpen&&<div className='content-box'>{children}</div>}
         </div>
     )
 }
